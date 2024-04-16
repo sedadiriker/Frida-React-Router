ac0d7ff892fa72d0b0581ac759e066c604d0bc69
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-16 17:14:04 +0300

    finishproject

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
         <iframe
         
           src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d120458.33594970636!2d-99.24486531231578!3d19.35516207166329!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x85d1ffc400ba3f3b%3A0x6201a4f7f7ccf4a1!2sLondres%20247%2C%20Del%20Carmen%2C%20Coyoac%C3%A1n%2C%2004100%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX%2C%20Meksika!3m2!1d19.3551806!2d-99.1624636!5e0!3m2!1str!2str!4v1713266853702!5m2!1str!2str"
-          width="450"
+          width="320"
           height="250"
           style={{ border: "0" }}
           allowFullScreen=""
