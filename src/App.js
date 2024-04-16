50430489ea4a09b951e74e3eca48fd296dafb0e4
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-16 10:41:05 +0300

    router component create

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.scss"
+import Approuter from "./router/router";
 function App() {
   return (
     <div className="App">
-      
+      <Approuter/>
     </div>
   );
 }
---
+++ src/router/router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Navbar from "../components/Navbar/Navbar"
+import Home from "../pages/home/Home"
+import About from "../pages/about/About"
+import Contact from "../pages/contact/Contact"
+import Projects from "../pages/projects/Projects"
+import NotFound from "../components/Error/NotFound"
+
+const Approuter = () => {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Approuter
