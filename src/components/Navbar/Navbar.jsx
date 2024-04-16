cd9a09a0db3b50291cec3f971bb217c2208c8ea6
Seda Diriker <146026945+sedadiriker@users.noreply.github.com>
2024-04-16 10:53:33 +0300

    navbar component fnish

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
-
+import { NavLink } from "react-router-dom"
+import "./Navbar.scss"
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className="nav">
+      <ul className="navbar">
+        <NavLink className="home">Home</NavLink>
+        <NavLink className="about">About</NavLink>
+        <NavLink className="projects">Projects</NavLink>
+        <NavLink className="contact">Contact</NavLink>
+      </ul>
     </div>
   )
 }
